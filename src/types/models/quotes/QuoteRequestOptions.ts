39de6b1a94d797ef1a8e9c06ed7f5f3878642b9e
alexclark_nz <hello@alexclark.co.nz>
2022-07-06 14:36:34 +1200

    Fix typo

--- src/types/models/quotes/QuoteRequestOptions.ts
+++ src/types/models/quotes/QuoteRequestOptions.ts
@@ -1,15 +1,15 @@
 export interface QuoteRequestOptions {
-    club_options: SimpleOption;
-    driver_licence_options: SimpleOption;
-    incident_options: SimpleOption;
-    insurance_companies: SimpleOption;
-    level_of_insurance_options: SimpleOption;
-    policy_description_options: SimpleOption;
-    referrer_category_options: SimpleOption;
-    relationship_options: SimpleOption;
-    storage_location_options: SimpleOption;
-    vehicle_usage_options: SimpleOption;
-    policy_benefit_template_options: SimpleOption;
+    club_options: SimpleOption[];
+    driver_licence_options: SimpleOption[];
+    incident_options: SimpleOption[];
+    insurance_companies: SimpleOption[];
+    level_of_insurance_options: SimpleOption[];
+    policy_description_options: SimpleOption[];
+    referrer_category_options: SimpleOption[];
+    relationship_options: SimpleOption[];
+    storage_location_options: SimpleOption[];
+    vehicle_usage_options: SimpleOption[];
+    policy_benefit_template_options: SimpleOption[];
     referrer_options: {
         id: number;
         name: string;
