da078e3884c6545c7482913a044ec9d4710d618c
alexclark_nz <hello@alexclark.co.nz>
2022-07-15 16:16:27 +1200

    Adjust benefits and enhancements to suit api changes

--- src/types/models/quotes/PolicyBenefit.ts
+++ src/types/models/quotes/PolicyBenefit.ts
@@ -1,4 +1,7 @@
 export interface PolicyBenefit {
     id: number;
     name: string | null;
+    description: string | null;
+    sort_order: number | null;
+    icon: string | null;
 }
--- src/types/models/quotes/PolicyEnhancement.ts
+++ src/types/models/quotes/PolicyEnhancement.ts
@@ -1,14 +1,11 @@
-import { PremiumType, QuoteRequestPurchaseOption } from "./QuoteRequestPurchaseOption";
+import { QuoteRequestPurchaseOption } from "./QuoteRequestPurchaseOption";
 
 export interface PolicyEnhancement {
     id: number;
     name: string | null;
     description: string | null;
     premium: number | null;
-    monthly_premium: number | null;
     disable_rounding: boolean | null;
     auto_select: boolean | null;
-    show_monthly: boolean | null;
-    premium_type: PremiumType | null;
     purchase_option_id: QuoteRequestPurchaseOption['id'] | null;
 }
