39543f0042bb323250e7d93face9b0556446f70c
alexclark_nz <hello@alexclark.co.nz>
2022-07-27 14:05:58 +1200

    Update info request interfaces as per api changes

--- src/types/models/quotes/InformationRequest.ts
+++ src/types/models/quotes/InformationRequest.ts
@@ -18,19 +18,21 @@ export interface InformationRequestMessage {
     id: number;
     created_at: string;
     updated_at: string;
-    quote_information_request_id: string;
+    information_request_id: string;
     user_id: number | null;
     content: string;
     has_been_read_by_customer: boolean;
+    sent: boolean;
+    received: boolean;
     user?: User;
+    attachments?: InformationRequestAttachment[];
 }
 
 export interface InformationRequestAttachment {
     id: number;
     created_at: string;
     updated_at: string;
-    quote_information_request_message_id: string;
+    information_request_message_id: string;
     title: string;
     url: string;
-    disk: string | null;
 }
