aa87672fff15ed7ad908a0b069ebe39617fb3f22
alexclark_nz <hello@alexclark.co.nz>
2022-08-25 11:50:55 +1200

    WIP: Policy benefit type updates

--- src/types/models/quotes/PolicyBenefit.ts
+++ src/types/models/quotes/PolicyBenefit.ts
@@ -1,16 +1,16 @@
 export interface PolicyBenefit {
-    id: number;
-    name: string;
-    description: string;
-    sort_order: number;
-    icon: string;
-    template_id: PolicyBenefitTemplate['id'];
+    id?: number;
+    name?: string;
+    description?: string;
+    sort_order?: number;
+    icon?: string;
+    template_id?: PolicyBenefitTemplate['id'];
 }
 
 export interface PolicyBenefitTemplate {
-    id: number;
-    created_at: string;
-    updated_at: string;
-    name: string;
-    sort_order: number;
+    id?: number;
+    created_at?: string;
+    updated_at?: string;
+    name?: string;
+    sort_order?: number;
 }
