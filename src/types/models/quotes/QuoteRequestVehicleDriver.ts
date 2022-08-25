0b2f8715cf2e746ed0d1b71072f05e16463a7d1d
alexclark_nz <hello@alexclark.co.nz>
2022-08-25 12:15:14 +1200

    Make driver fields optional

--- src/types/models/quotes/QuoteRequestVehicleDriver.ts
+++ src/types/models/quotes/QuoteRequestVehicleDriver.ts
@@ -1,10 +1,10 @@
 export interface QuoteRequestVehicleDriver {
-    id: number;
-    first_name: string | null;
-    last_name: string | null;
-    dob: string | null;
-    relationship: string | null;
-    licence: string | null;
-    licence_other: string | null;
-    quote_request_vehicle_id: number;
+    id?: number;
+    first_name?: string | null;
+    last_name?: string | null;
+    dob?: string | null;
+    relationship?: string | null;
+    licence?: string | null;
+    licence_other?: string | null;
+    quote_request_vehicle_id?: number;
 }
