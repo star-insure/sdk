a187e5bdc04efe2f694df71d72ee065b1d8f9202
alexclark_nz <hello@alexclark.co.nz>
2022-09-22 10:58:09 +1200

    Add user context field to role

--- src/types/models/auth/Role.ts
+++ src/types/models/auth/Role.ts
@@ -1,7 +1,9 @@
 import { Permission } from ".";
+import { UserContext } from "../../api";
 
 export interface Role {
     id: number;
     name: string;
+    context: UserContext;
     permissions?: Permission[];
 }
