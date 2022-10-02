855b04d22aae8f2b11c97a120110488e074e8052
Alex Clark <hello@alexclark.co.nz>
2022-10-03 10:17:13 +1300

    Refine date formatting cause safari is crap

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -1,11 +1,22 @@
-import { format } from "date-fns";
+import { format, parse } from "date-fns";
+
+export function parseDate(dateString: string): Date {
+    // Make sure we've only got 10 characters
+    return parse(dateString.substring(0, 10), 'yyyy-MM-dd', new Date());
+}
+
+export function parseDateTime(dateTimeString: string): Date {
+    return parse(dateTimeString, 'yyyy-MM-dd HH:mm:ss', new Date());
+}
 
 /**
  * Formats a date like "Jan 1, 2022 at 9:50am"
  */
 export function formatDateNice(date: string, includeTime: boolean = true) {
     const formatString = includeTime ? "MMM d, yyyy 'at' h:mma" : 'MMM d, yyyy';
-    return format(new Date(date), formatString);
+    const parsed = includeTime ? parseDateTime(date) : parseDate(date);
+
+    return format(parsed, formatString);
 }
 
 /**
@@ -13,5 +24,7 @@ export function formatDateNice(date: string, includeTime: boolean = true) {
  */
 export function formatDateForTable(date: string, includeTime: boolean = true) {
     const formatString = includeTime ? 'dd/MM/yyyy h:mma' : 'dd/MM/yyyy';
-    return format(new Date(date), formatString);
+    const parsed = includeTime ? parseDateTime(date) : parseDate(date);
+
+    return format(parsed, formatString);
 }
