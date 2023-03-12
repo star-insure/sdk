c9661950ac527b36dd8eb596e28dea3eca469d14
nzbasic <jamescoppard024@gmail.com>
2023-03-13 11:39:17 +1300

    Add condensed option for tables

--- src/components/tables/TableCell.tsx
+++ src/components/tables/TableCell.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import cn from 'classnames';
 
 interface Props {
     children: React.ReactNode;
     className?: string;
+    condensed?: true;
 }
 
-export default function TableCell({ children, className = '' }: Props) {
+export default function TableCell({ children, className, condensed }: Props) {
     return (
-        <td className={`${className} px-3 py-3 text-sm text-gray-500`}>
+        <td className={cn(className, 'px-3 py-3 text-sm text-gray-500', {
+            'w-0 whitespace-nowrap': condensed,
+        })}>
             {children}
         </td>
     )
