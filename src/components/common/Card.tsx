acc9365f508221499bfa815f2729fe4a7149bf76
nzbasic <jamescoppard024@gmail.com>
2023-04-03 09:01:28 +1200

    Alter card style

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function Card({ className, children }: Props) {
     return (
-        <div className={`${className ?? ''} flex flex-col items-start bg-white rounded-md shadow border border-gray-200 p-4 md:p-6`}>
+        <div className={`${className ?? ''} flex flex-col items-start bg-white rounded-md shadow-sm border border-gray-300 p-4 md:p-6`}>
             {children}
         </div>
     );
