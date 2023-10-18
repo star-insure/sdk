f6020cacb5df76f0c17006c234e1e50347a57307
Alex Clark <hello@alexclark.co.nz>
2023-10-19 12:59:27 +1300

    Add storage_location_other to vehicle interface

--- src/types/models/quotes/QuoteRequestVehicle.ts
+++ src/types/models/quotes/QuoteRequestVehicle.ts
@@ -1,23 +1,24 @@
-import { Product, VehicleType } from "../../api";
-import { QuoteRequestVehicleDriver } from "./QuoteRequestVehicleDriver";
+import { Product, VehicleType } from '../../api';
+import { QuoteRequestVehicleDriver } from './QuoteRequestVehicleDriver';
 
 export interface QuoteRequestVehicle {
-    id?: number;
-    vehicle_type?: VehicleType;
-    registration?: string;
-    make?: string;
-    model?: string;
-    year?: string;
-    usage?: string;
-    product?: Product;
-    has_financially_interested_party?: boolean;
-    financially_interested_party_detail?: string;
-    owned_duration?: string;
-    value?: number;
-    storage_location?: string;
-    is_heavy?: boolean;
-    quote_request_id?: string;
-    drivers?: QuoteRequestVehicleDriver[];
-    key?: string;
-    main_driver?: string;
+  id?: number;
+  vehicle_type?: VehicleType;
+  registration?: string;
+  make?: string;
+  model?: string;
+  year?: string;
+  usage?: string;
+  product?: Product;
+  has_financially_interested_party?: boolean;
+  financially_interested_party_detail?: string;
+  owned_duration?: string;
+  value?: number;
+  storage_location?: string;
+  storage_location_other?: string;
+  is_heavy?: boolean;
+  quote_request_id?: string;
+  drivers?: QuoteRequestVehicleDriver[];
+  key?: string;
+  main_driver?: string;
 }
