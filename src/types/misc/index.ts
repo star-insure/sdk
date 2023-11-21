d9127ffe5b01093bac0c21cfa92446a057acd6a0
Alex Clark <hello@alexclark.co.nz>
2023-11-21 14:10:44 +1300

    Add datalist

--- src/types/misc/index.ts
+++ src/types/misc/index.ts
@@ -11,7 +11,7 @@ export interface FilterOption {
     label: string;
     name: string;
     options?: FilterValue[];
-    type?: 'options' | 'date' | 'greaterThan' | 'scope' | 'select';
+    type?: 'options' | 'date' | 'greaterThan' | 'scope' | 'select' | 'datalist';
 }
 
 export interface FilterValue {
