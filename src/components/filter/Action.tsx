a2b2c66dd100abc134d78da7d2737ad89eeb0b42
Alex Clark <hello@alexclark.co.nz>
2023-12-06 21:57:45 +1300

    Add tooltips to actions with shortcutKeys

--- src/components/filter/Action.tsx
+++ src/components/filter/Action.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Tooltip } from 'react-tooltip';
 import { Link, router } from "@inertiajs/react";
 import { TPageHeaderAction } from "../../types";
 
 export default function Action({ title, href, as = 'Link', target = '_self', type, onClick = () => {}, shortcutKey }: TPageHeaderAction) {
     const className =
         'bg-white rounded-full font-bold px-4 py-1.5 text-sm whitespace-nowrap hover:bg-gray-100 hover:border-gray-400 transition-colors border border-gray-300';
+
+    const tooltipId = `action-${title}`;
+    const tooltipContent = shortcutKey ? `Ctrl + ${shortcutKey}` : undefined;
 
     function runAction() {
         if (as === 'Link' && href) {
@@ -41,23 +45,32 @@ export default function Action({ title, href, as = 'Link', target = '_self', typ
 
     if (as === 'Link' && href) {
         return (
-            <Link className={className} href={href} onClick={() => onClick()}>
-                {title}
-            </Link>
+            <>
+                {tooltipContent && <Tooltip id={tooltipId} className="z-10" />}
+                <Link data-tooltip-id={tooltipId} data-tooltip-content={tooltipContent} className={className} href={href} onClick={() => onClick()}>
+                    {title}
+                </Link>
+            </>
         );
     }
 
     if (as === 'a' && href) {
         return (
-            <a className={className} target={target} href={href} onClick={() => onClick()}>
-                {title}
-            </a>
+            <>
+                {tooltipContent && <Tooltip id={tooltipId} className="z-10" />}
+                <a data-tooltip-id={tooltipId} data-tooltip-content={tooltipContent} className={className} target={target} href={href} onClick={() => onClick()}>
+                    {title}
+                </a>
+            </>
         );
     }
 
     return (
-        <button className={className} type={type} onClick={() => onClick()}>
-            {title}
-        </button>
+        <>
+            {tooltipContent && <Tooltip id={tooltipId} className="z-10" />}
+            <button data-tooltip-id={tooltipId} data-tooltip-content={tooltipContent} className={className} type={type} onClick={() => onClick()}>
+                {title}
+            </button>
+        </>
     );
 }
