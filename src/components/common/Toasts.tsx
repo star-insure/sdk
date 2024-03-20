8c0219174b92ae6062653f00a71e353941f73698
Alex Clark <hello@alexclark.co.nz>
2024-03-20 14:39:21 +1300

    Make toast a higher z-index than modals

--- src/components/common/Toasts.tsx
+++ src/components/common/Toasts.tsx
@@ -7,7 +7,7 @@ export default function Toasts() {
     const { toasts } = useToast();
 
     return (
-        <div className="fixed bottom-4 right-4 z-[110] flex flex-col gap-6">
+        <div className="fixed bottom-4 right-4 z-[120] flex flex-col gap-6">
             {toasts.map((toast: Toast) => (
                 <ToastItem {...toast} key={toast._id} />
             ))}
