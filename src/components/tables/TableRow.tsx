afecd434a1215108e9054f73200fa9d33c943dfe
Alex Clark <hello@alexclark.co.nz>
2024-03-21 14:14:37 +1300

    Add event param to onclick

--- src/components/tables/TableRow.tsx
+++ src/components/tables/TableRow.tsx
@@ -4,14 +4,14 @@ import cn from 'classnames';
 interface Props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLTableRowElement>, HTMLTableRowElement> {
     children: React.ReactNode;
     className?: string;
-    onClick?: () => void;
+    onClick?: (e?: React.SyntheticEvent) => void;
 }
 
 export default function TableRow({ children, className = '', onClick = () => {}, ...props }: Props) {
     const bgClass = className.includes('bg') ? '' : 'bg-white even:bg-gray-50'
 
     return (
-        <tr {...props} className={cn(className, bgClass, 'hover:bg-gray-100')} onClick={onClick}>
+        <tr {...props} className={cn(className, bgClass, 'hover:bg-gray-100')} onClick={(e) => onClick(e)}>
             {children}
         </tr>
     )
