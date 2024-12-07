23c4c428d6c0a8bd4ab3780a938c55d5bd017b43
Alex Clark <hello@alexclark.co.nz>
2024-12-08 07:32:30 +1300

    2FA types

--- src/types/api/auth.ts
+++ src/types/api/auth.ts
@@ -1,41 +1,45 @@
 export type UserContext =
-  | 'administrator'
-  | 'staff'
-  | 'broker'
-  | 'dealer'
-  | 'agent'
-  | 'security'
-  | 'customer';
+    | 'administrator'
+    | 'staff'
+    | 'broker'
+    | 'dealer'
+    | 'agent'
+    | 'security'
+    | 'customer';
 
 type AuthUser = {
-  id: number;
-  name: string;
-  email: string;
-  is_group_admin: boolean;
-  umbrella_filter?: string;
-  oracle_user_id?: string;
-  groups: AuthGroup[];
-  phone_extension?: number;
+    id: number;
+    name: string;
+    email: string;
+    is_group_admin: boolean;
+    umbrella_filter?: string;
+    oracle_user_id?: string;
+    groups: AuthGroup[];
+    phone_extension?: number;
+    two_factor_expires_at?: string;
+    two_factor_method?: TwoFactorAuthMethod;
 };
 
 type AuthGroup = {
-  id: number;
-  name: string;
-  code: string;
-  role: AuthRole;
+    id: number;
+    name: string;
+    code: string;
+    role: AuthRole;
 };
 
 type AuthRole = {
-  name: string;
-  context: string;
+    name: string;
+    context: string;
 };
 
 export interface AuthContext {
-  user?: AuthUser;
-  context: UserContext;
-  group?: AuthGroup;
-  permissions: string[];
-  is_app?: boolean;
-  can: (permission: string) => boolean;
-  should_2fa?: boolean;
+    user?: AuthUser;
+    context: UserContext;
+    group?: AuthGroup;
+    permissions: string[];
+    is_app?: boolean;
+    can: (permission: string) => boolean;
+    should_two_factor_auth: boolean;
 }
+
+type TwoFactorAuthMethod = 'email' | 'sms';
