e68903768f24fd90d6ee5d67d1d7bcb5f8dadce6
Alex Clark <hello@alexclark.co.nz>
2025-04-08 14:15:15 +1200

    Add form attribute to button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,79 +2,81 @@ import React from 'react';
 import { Link } from '@inertiajs/react';
 
 interface Props {
-  className?: string;
-  href?: string;
-  target?: '_blank' | '_self';
-  onClick?: Function;
-  type?: 'button' | 'submit';
-  status?: 'primary' | 'danger' | 'warning' | 'info' | 'default';
-  children: React.ReactNode;
-  disabled?: boolean;
-  as?: 'link' | 'button' | 'a';
-  small?: boolean;
+    className?: string;
+    href?: string;
+    target?: '_blank' | '_self';
+    onClick?: Function;
+    type?: 'button' | 'submit';
+    status?: 'primary' | 'danger' | 'warning' | 'info' | 'default';
+    children: React.ReactNode;
+    disabled?: boolean;
+    as?: 'link' | 'button' | 'a';
+    small?: boolean;
+    form?: string;
 }
 
 export default function Button({
-  className,
-  href,
-  target,
-  onClick,
-  type,
-  children,
-  status = 'default',
-  disabled = false,
-  as,
-  small = false,
+    className,
+    href,
+    target,
+    onClick,
+    type,
+    children,
+    status = 'default',
+    disabled = false,
+    as,
+    small = false,
+    form,
 }: Props) {
-  const baseClasses = small
-    ? 'font-bold text-sm inline-flex items-center gap-3 justify-center text-white text-center px-3 py-1 rounded min-w-[80px] transition-all hover:brightness-110'
-    : 'font-black inline-flex items-center gap-3 justify-center text-white text-center px-5 py-2 rounded-md min-w-[120px] transition-all hover:brightness-110';
+    const baseClasses = small
+        ? 'font-bold text-sm inline-flex items-center gap-3 justify-center text-white text-center px-3 py-1 rounded min-w-[80px] transition-all hover:brightness-110'
+        : 'font-black inline-flex items-center gap-3 justify-center text-white text-center px-5 py-2 rounded-md min-w-[120px] transition-all hover:brightness-110';
 
-  const statusClass =
-    (status === 'primary' && 'bg-accent') ||
-    (status === 'danger' && 'bg-red-500') ||
-    (status === 'warning' && 'bg-yellow-400') ||
-    (status === 'info' && 'bg-blue-400') ||
-    'bg-gray-600';
+    const statusClass =
+        (status === 'primary' && 'bg-accent') ||
+        (status === 'danger' && 'bg-red-500') ||
+        (status === 'warning' && 'bg-yellow-400') ||
+        (status === 'info' && 'bg-blue-400') ||
+        'bg-gray-600';
 
-  const classes = `${className ?? ''} ${baseClasses} ${statusClass}`;
+    const classes = `${className ?? ''} ${baseClasses} ${statusClass}`;
 
-  if (onClick) {
-    return (
-      <button
-        className={classes}
-        type={type ?? 'button'}
-        onClick={(e: React.MouseEvent) => onClick(e)}
-        disabled={disabled}
-      >
-        {children}
-      </button>
-    );
-  }
-
-  if (type) {
-    return (
-      <button className={classes} type={type ?? 'button'} disabled={disabled}>
-        {children}
-      </button>
-    );
-  }
-
-  if (href) {
-    if (href.includes('http') || target === '_blank' || as === 'a') {
-      return (
-        <a href={href} target={target ?? '_self'} className={classes}>
-          {children}
-        </a>
-      );
+    if (onClick) {
+        return (
+            <button
+                className={classes}
+                type={type ?? 'button'}
+                onClick={(e: React.MouseEvent) => onClick(e)}
+                disabled={disabled}
+            >
+                {children}
+            </button>
+        );
     }
 
-    return (
-      <Link href={href} className={classes}>
-        {children}
-      </Link>
-    );
-  }
+    if (type) {
+        return (
+            <button form={form} className={classes} type={type ?? 'button'} disabled={disabled}>
+                {children}
+            </button>
+        );
+    }
 
-  return <p className="text-red-500 text-sm">[Invalid button]</p>;
+    if (href) {
+        if (href.includes('http') || target === '_blank' || as === 'a') {
+            return (
+                <a href={href} target={target ?? '_self'} className={classes}>
+                    {children}
+                </a>
+            );
+        }
+
+        return (
+            <Link href={href} className={classes}>
+                {children}
+            </Link>
+        );
+    }
+
+    return <p className="text-red-500 text-sm">[Invalid button]</p>;
 }
